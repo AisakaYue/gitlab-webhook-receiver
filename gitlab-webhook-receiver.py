31131a5e4282f4c291f66211a5f31fd04b89688e
dragonkid <idragonkid@gmail.com>
2016-07-07 14:26:45 +0800

    support notify through email & refactor popen with sh

--- gitlab-webhook-receiver.py
+++ gitlab-webhook-receiver.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 import os
 import json
 import logging
-import subprocess
-import logging.handlers
+from logging import handlers
 import threading
 from Queue import Queue
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
@@ -12,10 +11,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 log_file = 'webhook.log'
 log_level = logging.DEBUG      # DEBUG is quite verbose
-queue = Queue()
-
-
-logger = logging.getLogger('log')
+logger = logging.getLogger('gitlab-webhook')
 logger.setLevel(log_level)
 log_handler = logging.handlers.RotatingFileHandler(log_file, backupCount=4)
 f = logging.Formatter("%(asctime)s %(filename)s %(levelname)s %(message)s",
@@ -23,26 +19,66 @@ f = logging.Formatter("%(asctime)s %(filename)s %(levelname)s %(message)s",
 log_handler.setFormatter(f)
 logger.addHandler(log_handler)
 
+queue = Queue()
+
+options = None
+
+
+def send_email(subject, content):
+    import smtplib
+    from email.header import Header
+    from email.mime.text import MIMEText
+
+    message = MIMEText(content, 'plain', 'utf-8')
+    message['From'] = options.email_sender
+    message['To'] = options.email_receivers
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        smtpObj = smtplib.SMTP(host=options.email_server)
+        smtpObj.login(options.email_user, options.email_passwd)
+        smtpObj.sendmail(
+            options.email_sender,
+            options.email_receivers.split(','),
+            message.as_string()
+        )
+    except smtplib.SMTPException as e:
+        logger.exception('email send failed.\n%r', e)
+
 
 def clone_project(git_ssh_url, branch):
-    cmd = 'cd /tmp && git clone -b {} {}'.format(branch, git_ssh_url)
-    logger.debug(cmd)
-    proc = subprocess.Popen(cmd, shell=True)
-    proc.wait()
+    from sh import git
+    os.chdir('/tmp')
+    p = git('clone', '-b', branch, git_ssh_url)
+    p.wait()
+    logger.info(p.cmd)
 
 
 def do_something(project_name, object_kind='tag_push'):
-    cmd = 'TRIGGER={} /bin/bash /tmp/{}/packaging.sh'.format(object_kind, project_name)
-    logger.debug('do something: %s', cmd)
-    proc = subprocess.Popen(cmd, shell=True)
-    proc.wait()
+    from sh import bash
+    try:
+        p = bash(
+            '/tmp/{project}/packaging.sh'.format(project=project_name),
+            _env={'TRIGGER': object_kind}
+        )
+        logger.info('do something: {cmd}'.format(cmd=p.cmd))
+        p.wait()
+        subject = '{proj} CI Success.'.format(proj=project_name)
+        content = p.stdout
+    except Exception as e:  # unknown exceptions
+        subject = '{proj} CI Failed.'.format(proj=project_name)
+        content = repr(e)
+
+    logger.info('{subject}\n{content}'.format(subject=subject, content=content))
+    if options.email_notify:
+        send_email(subject, content)
 
 
 def clean_project(project_name):
-    logger.debug('clean project')
-    proc = subprocess.Popen('sudo rm -rf /tmp/{}'.format(project_name), shell=True)
-    proc.wait()
-
+    from sh import sudo, rm
+    logger.info('clean project')
+    with sudo:
+        rm('-rf', '/tmp/{}'.format(project_name)).wait()
 
 def parse_single_post(data_string):
     logger.info('start parsing data_string')
@@ -55,17 +91,18 @@ def parse_single_post(data_string):
     # get ssh url
     git_ssh_url = post_msg['repository']['git_ssh_url']
     logger.debug(git_ssh_url)
+
+    project_name = post_msg['repository']['name']
+    logger.info(project_name)
+    clean_project(project_name)
+
     # get the real branch. refs/tags/1.0.0 => 1.0.0
     # refs/heads/enhancement/auto-packaging-test-debs => enhancement/auto-packaging-test-debs
     branch = '/'.join(post_msg['ref'].split('/')[2:])
     logger.debug(branch)
-
     clone_project(git_ssh_url, branch)
 
-    project_name = post_msg['repository']['name']
-    logger.debug(project_name)
     do_something(project_name, object_kind)
-
     clean_project(project_name)
 
     logger.info('parsing finished')
@@ -96,15 +133,36 @@ class webhookReceiver(BaseHTTPRequestHandler):
         queue.put(data_string)
 
 
+def parse_cmdline():
+    import argparse
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '-p', '--port', default=80000, help='port of the service listening'
+    )
+    argparser.add_argument(
+        '--email-notify', dest='email_notify', action='store_true',
+        help='notify the result of packaging.sh through email when it run failed'
+    )
+    argparser.add_argument('--email-server', dest='email_server')
+    argparser.add_argument('--email-user', dest='email_user')
+    argparser.add_argument('--email-passwd', dest='email_passwd')
+    argparser.add_argument('--email-sender', dest='email_sender')
+    argparser.add_argument('--email-receivers', dest='email_receivers', help='split with comma')
+    return argparser.parse_args()
+
+
 def main():
     """
         the main event.
     """
+    global options
+    options = parse_cmdline()
+
     t = threading.Thread(target=parse_forever)
     t.setDaemon(True)
     t.start()
 
-    server = HTTPServer(('', 8000), webhookReceiver)
+    server = HTTPServer(('', int(options.port)), webhookReceiver)
     try:
         logger.info('started web server...')
         server.serve_forever()
